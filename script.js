a3931088659f33153e595ea500714f7f8916ec7d
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2023-06-02 18:40:36 +0300

    task 1#4

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ function initMap() {
         ["R", "P"," ", " "," ", " ","p", "r"]
     ];
 }
+
+function figureToHtml(figure) {
+    switch (figure) {
+        case "K": return "&#9812"; case "k": return "&#9818";
+        case "Q": return "&#9813"; case "q": return "&#9819";
+        case "R": return "&#9814"; case "r": return "&#9820";
+        case "B": return "&#9815"; case "b": return "&#9821";
+        case "N": return "&#9816"; case "n": return "&#9822";
+        case "P": return "&#9817"; case "p": return "&#9823";
+        default: return "&nbsp;";
+    }
+}
+
 function showMap() {
     let html = "<table border='1' cellpadding='2' cellspacing='0'>";
     for (let y = 7; y >= 0;  y--) {
@@ -21,8 +34,12 @@ function showMap() {
         html += "<td>&nbsp;" + y + "&nbsp;</td>";
         for (let x = 0; x <= 7; x++) {
             let colorBg = (x + y) % 2 ?  "#eeffee" : "#abcdef";
-            html += "<td style='height: 50px; width: 50px; background-color: " + colorBg + "; text-align: center'>";
-            html += map[x][y];
+            html += "<td style=" +
+                "'height: 50px;" +
+                "width: 50px; background-color: " + colorBg + "; " +
+                "text-align: center; font-size: 40px;" +
+                "'>";
+            html += figureToHtml(map[x][y]);
             html += "</td>";
         }
         html += "</tr>";
